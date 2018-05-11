db70e79e38374994f9d2fa878d705abf70e62903
duyuanhao <13775516233@163.com>
2018-05-11 16:34:30 +0800

    demo

--- src/main/java/com/example/demo/demo/HelloController.java
+++ src/main/java/com/example/demo/demo/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.demo.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    @Value("${abc}")
+    private int a;
+
+    @Autowired
+    private CounterService counterService;
 
     @RequestMapping("hello")
     public String index(){
-        return "hello12";
+        System.out.println(a);
+        return "hello1 world";
+    }
+    @RequestMapping("hello2")
+    public String greet(){
+        counterService.increment("didispace.hello.count");
+        return "";
     }
 }
--- src/test/java/com/example/demo/TestDemo.java
+++ src/test/java/com/example/demo/TestDemo.java
@@ -5,10 +5,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @Author: dyh
@@ -27,5 +33,9 @@ public class TestDemo {
     }
 
     @Test
-    public void 
+    public void hello()throws  Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("hello1 world")));
+    }
 }
