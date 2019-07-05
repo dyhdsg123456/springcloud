68f8b7f24894c95d86d16daa0fa263a1cb74f59c
duyuanhao <13775516233@163.com>
2019-07-05 17:41:06 +0800

    1

--- src/main/java/com/example/demo/demo/HelloController.java
+++ src/main/java/com/example/demo/demo/HelloController.java
@@ -1,10 +1,16 @@
 package com.example.demo.demo;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * @Author: dyh
@@ -13,17 +19,29 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    private final Logger          logger =Logger.getLogger(getClass());
     @Value("${abc}")
-    private int a;
+    private        int             a;
+    @Autowired
+    private        DiscoveryClient client;
 
-    
+    @Qualifier("counterService")
     @Autowired
     private CounterService counterService;
 
 
     @RequestMapping("hello")
-    public String index(){
-        System.out.println(a);
+    public String index()throws Exception{
+        long start = System.currentTimeMillis();
+
+
+        ServiceInstance instance = client.getLocalServiceInstance();
+        int sleeptime = new Random().nextInt(3000);
+        logger.info("sleeptime:"+sleeptime);
+        Thread.sleep(sleeptime);
+        logger.info("/hello,host: "+instance.getHost()+",serviceid: "+instance.getServiceId());
+        long end = System.currentTimeMillis();
+        logger.info("spendtime:"+(end-start));
         return "hello world";
     }
     @RequestMapping("hello2")
